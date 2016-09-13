f1984f7713a5a190a5eec7856c51941d5fcbe831
Angela M. Choi <angela.minchoi@gmail.com>
2016-09-13 15:55:14 -0400

    finished orders and feedback routers

--- server/app/routes/feedback.js
+++ server/app/routes/feedback.js
@@ -1,5 +1,54 @@
 'use strict';
-var router = require('express').Router(); // eslint-disable-line new-cap
+var router = require('express').Router();
+var HTTP_Error = require('../utils').HTTP_Error;
 module.exports = router;
 
 var Feedback = require('../../db/models/feedback.js');
+var User = require('../../db/models/user.js');
+
+router.get('/', function (req, res, next) {
+	Feedback.findAll()
+	.then(function(allFeedback) {
+		res.json(allFeedback);
+	})
+	.catch(next);
+
+});
+
+router.post('/', function (req, res, next) {
+    User.findById({
+        where: {id: req.user.id}
+    })
+    .spread(function (user, wasCreatedBool) {
+        return Feedback.create(req.body)
+        .then(function (createdFeedback) {
+            return createdFeedback.setUser(user);
+        });
+    })
+    .then(function (createdFeedback) {
+        res.sendStatus(200);
+    })
+    .catch(next);
+
+});
+//Feedback.belongsTo(User); in index.js
+
+router.get('/:id', function (req, res, next) {
+	Feedback.findById(req.params.id)
+	.then(function (feedback) {
+		if (!feedback) throw HTTP_Error(404, 'feedback not found');
+		res.json(feedback);
+	})
+	.catch(next);
+});
+
+router.delete('/:id', function (req, res, next) {
+	Feedback.destroy({
+		where: {id: req.params.id}
+	})
+	.then(function(feedbackDestroyed) {
+		if (!feedbackDestroyed) throw HTTP_Error(404, 'feedback not destroyed');
+		res.sendStatus(204);
+	})
+	.catch(next);
+});
--- server/app/routes/orders.js
+++ server/app/routes/orders.js
@@ -1,32 +1,34 @@
 'use strict';
-var router = require('express').Router(); // eslint-disable-line new-cap
-const HTTP_Error = require('../utils').HTTP_Error;
+var router = require('express').Router();
+var HTTP_Error = require('../utils').HTTP_Error;
 module.exports = router;
 
 var Order = require('../../db/models/order.js');
 
 router.get('/', function (req, res, next) {
-  Order.findAll()
-  .then(orders => res.json(orders))
-  .catch(next);
+	Order.findAll()
+	.then(function(orders) {
+		res.json(orders);
+	})
+	.catch(next);
 });
 
 router.get('/:id', function (req, res, next) {
-  Order.findById(req.params.id)
-  .then(order => {
-    if (!order) throw HTTP_Error(404, 'order not found');
-    res.json(order);
-  })
-  .catch(next);
+	Order.findById(req.params.id)
+	.then(function(order) {
+		if (!order) throw HTTP_Error(404, 'order not found');
+		res.json(order);
+	})
+	.catch(next);
 });
 
 router.delete('/:id', function (req, res, next) {
-  Order.destroy({
-    where: {id: req.params.id}
-  })
-  .then(orderDestroyed => {
-    if (!orderDestroyed) throw HTTP_Error(404, 'no order destroyed');
-    res.sendStatus(204);
-  })
-  .catch(next);
+	Order.destroy({
+		where: {id: req.params.id}
+	})
+	.then(function(orderDestroyed) {
+		if (!orderDestroyed) throw HTTP_Error(404, 'order not destroyed');
+		res.sendStatus(204);
+	})
+	.catch(next);
 });
