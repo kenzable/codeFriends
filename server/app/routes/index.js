bfd2c0c92633126dab59c58eb6161ddb3cc034b4
Mackenzie Turner <turner.mackenzie.m@gmail.com>
2016-09-13 15:57:22 -0400

    added some routes;

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,9 +2,6 @@
 var router = require('express').Router(); // eslint-disable-line new-cap
 module.exports = router;
 
-// router.use('/members', require('./members'));
-
-
 // Make sure this is after all of
 // the registered routes!
 router.use(function (req, res) {
