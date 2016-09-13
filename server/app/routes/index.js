58453cdf1a967528c3b178d07faf71667a925cb3
Mackenzie Turner <turner.mackenzie.m@gmail.com>
2016-09-13 11:57:40 -0400

    nevermind I screwed up...

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,8 +3,7 @@ var router = require('express').Router(); // eslint-disable-line new-cap
 module.exports = router;
 
 // router.use('/members', require('./members'));
-router.use('/api', require('./api'));
-router.use('/auth', require('./auth'));
+
 
 // Make sure this is after all of
 // the registered routes!
