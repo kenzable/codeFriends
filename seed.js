e7f54722a1d3d5ac41b796bbe174cfaf34a997b0
Danielle YoungSmith <dyoungsmith@gmail.com>
2016-09-14 18:28:08 -0400

    fake info set up, wrestling with pg

--- seed.js
+++ seed.js
@@ -1,63 +1,81 @@
-/*
-
-This seed file is only a placeholder. It should be expanded and altered
-to fit the development of your application.
-
-It uses the same file the server uses to establish
-the database connection:
---- server/db/index.js
-
-The name of the database used is set in your environment files:
---- server/env/*
-
-This seed file has a safety check to see if you already have users
-in the database. If you are developing multiple applications with the
-fsg scaffolding, keep in mind that fsg always uses the same database
-name in the environment files.
-
-*/
+'use strict';
 
 var chalk = require('chalk');
+var Promise = require('sequelize').Promise;
+var faker = require('./Faker/faker');
+
 var db = require('./server/db');
 var User = db.model('user');
-var Cart = db.model('cart');
-var Feedback = db.model('feedback');
 var Friend = db.model('friend');
+var Cart = db.model('cart');
 var Order = db.model('order');
-var Promise = require('sequelize').Promise;
+var Feedback = db.model('feedback');
 
-var seedUsers = function () {
+var randomEmail = faker.Internet.email();
+var randomWord = faker.Lorem.words();
+var randomName = faker.Name.findName();
+var randomNum = faker.random.number(100);
+var randomHandles = faker.Name.firstName();
 
-    var users = [
-        {
-            email: 'testing@fsa.com',
-            password: 'password',
-            name: 'Testing'
-        },
-        {
-            email: 'obama@gmail.com',
-            password: 'potus',
-            name: 'Obama'
-        }
-    ];
+// var randomDescription = faker.Lorem.paragraph();
+// var randomImg = faker.Image.animals();
 
-    var creatingUsers = users.map(function (userObj) {
-        return User.create(userObj);
-    });
 
-    return Promise.all(creatingUsers);
+var seedUser = function() {
+	var user = {
+		email: randomEmail,
+		password: 'randomWord',
+		name: randomName,
+		age: randomNum,
+		twitter_id: randomHandles,
+		facebook_id: randomHandles,
+		google_id: randomHandles
+	}
 
+	var UserPromises = [];
+	for (var i = 0; i < 15; i++) {
+		UserPromises.push(User.create(user))
+	}
+
+	return UserPromises;
 };
 
+// var seedFriend = function() {
+// 	var friend = {
+// 		name: randomName,
+// 		description: randomDescription,
+// 		numHours: randomNum,
+// 		price: randomNum,
+// 		imageUrl: randomImg,
+// 		tags: randomWord
+// 	}
+// };
+
+// var seedCart = function() {
+
+// };
+
+// var seedOrder = function() {
+
+// };
+
+// var seedFeedback = function() {
+
+// };
+
+// WILL HAVE TO CONCAT RETURNED PROMISE ARRAYS TO SEED ALL MODELS
+var seedPromises = seedUser();
+
 db.sync({ force: true })
-    .then(function () {
-        return seedUsers();
-    })
-    .then(function () {
-        console.log(chalk.green('Seed successful!'));
-        process.exit(0);
-    })
-    .catch(function (err) {
-        console.error(err);
-        process.exit(1);
-    });
+.then(function() {
+	Promise.all(seedPromises);
+})
+.then(function() {
+	console.log(chalk.green('seed successful'));
+})
+.catch(function(err) {
+	console.error(chalk.red(err));
+});
+
+
+
--- server/db/models/user.js
+++ server/db/models/user.js
@@ -8,8 +8,8 @@ var db = require('../_db');
 module.exports = db.define('user', {
     email: {
         type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
+        allowNull: false
+        // unique: true
     },
     password: {
         type: Sequelize.STRING,
