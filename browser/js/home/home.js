9f8ffb53c2373079d8e6142b2e58877c9bcbbf69
Angela Choi <angela.minchoi@gmail.com>
2016-09-16 10:51:48 -0400

    Fixing users js (#24)
    * linter warnings fixed #1
    
    * adding changes in about js
    
    * removed public main js

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -28,44 +28,8 @@ app.controller('CarouselCtrl', function ($scope, $log, ProductFactory) {
     });
   };
 
-  $scope.randomize = function() {
-    var indexes = generateIndexesArray();
-    assignNewIndexesToSlides(indexes);
-  };
-
   for (var i = 0; i < 4; i++) {
     $scope.addSlide();
   }
 
-  // Randomize logic below
-
-  function assignNewIndexesToSlides(indexes) {
-    for (var i = 0, l = slides.length; i < l; i++) {
-      slides[i].id = indexes.pop();
-    }
-  }
-
-  function generateIndexesArray() {
-    var indexes = [];
-    for (var i = 0; i < currIndex; ++i) {
-      indexes[i] = i;
-    }
-    return shuffle(indexes);
-  }
-
-  // http://stackoverflow.com/questions/962802#962890
-  function shuffle(array) {
-    var tmp, current, top = array.length;
-
-    if (top) {
-      while (--top) {
-        current = Math.floor(Math.random() * (top + 1));
-        tmp = array[current];
-        array[current] = array[top];
-        array[top] = tmp;
-      }
-    }
-
-    return array;
-  }
 });
