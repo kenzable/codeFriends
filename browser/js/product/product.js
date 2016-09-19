36426d58d070c8f3578e020d6f0165872f145c72
Danielle YoungSmith <dyoungsmith@gmail.com>
2016-09-19 16:12:28 -0400

    fixing product page: numRevs

--- browser/js/product/product.js
+++ browser/js/product/product.js
@@ -23,27 +23,62 @@ app.config(function ($stateProvider) {
 });
 
 
-app.controller('ProductController', function ($scope, ProductFactory, CartFactory, $log, $stateParams) {
+app.controller('ProductController', function ($scope, $q, ProductFactory, CartFactory, $log, $stateParams) {
 
     ProductFactory.getAllFriends()
-    .then(function(allFriends) {
-        $scope.allFriends = allFriends;
+
+    .then(function(friends) {
+        if (friends) {
+            $scope.allFriends = friends;
+            return $q.all($scope.allFriends.map(function(friend) {
+                return ProductFactory.getNumRevs(friend.id)
+            }))
+        }
     })
+    .then(function(feedback) {
+     for (var i = 0; i < feedback.length; i++) {
+         // Get number of reviews
+         allFriends[i].dataValues.numRevs = feedback[i].count;
+
+         // Get average rating
+         var friendRating = feedback[i].rows.map(function(row) {
+             return row.dataValues.rating;
+         });
+
+         var avgRating;
+
+         if (friendRating.length) {
+             var sum = friendRating.reduce(function(a, b) { return a + b});
+             avgRating = Math.floor(sum / friendRating.length);
+         }
+         else { avgRating = 0 }
+
+         allFriends[i].dataValues.avgRating = avgRating;
+     }
+     res.json(allFriends);
+    })
+
     .catch($log.error);
+
 
     $scope.id = $stateParams.friendId;
 
     $scope.getStars = ProductFactory.getStars;
 
-    $scope.getNumRevs = ProductFactory.getNumRevs;
-    $scope.getAvgRating = ProductFactory.getAvgRating;
+    // ProductFactory.getNumRevs()
+    // .then(function(numRevs) {
+    //     $scope.getNumRevs = numRevs
+    // })
+    // .catch($log.error);
+
+    // $scope.getAvgRating = ProductFactory.getAvgRating;
 
 
-    ProductFactory.getFriendReviews($scope.id)
-    .then(function(reviews) {
-        $scope.friendReviews = reviews.rows;
-    })
-    .catch($log.error)
+    // ProductFactory.getFriendReviews($scope.id)
+    // .then(function(reviews) {
+    //     $scope.friendReviews = reviews.rows;
+    // })
+    // .catch($log.error)
 
 
     ProductFactory.getFriend($scope.id)
