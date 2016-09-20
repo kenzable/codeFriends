a5e5b9c2bfa4a8dea814b53af29f5c1b7a039b24
Angela Choi <angela.minchoi@gmail.com>
2016-09-20 12:53:44 -0400

    Orderhistory (#43)
    * profile history working
    
    * deleted bower_components from github
    
    * deleted members only page and made user profile user only page
    
    * cleaned folders
    
    * api/orders/history/:id working
    
    * user order history working
    
    * order history, order by orderId
    
    * cleaned comments

--- browser/js/user/profile.js
+++ browser/js/user/profile.js
@@ -17,50 +17,18 @@ app.config(function ($stateProvider) {
 
 app.controller('ProfileController', function ($scope, ProfileFactory, AuthService) {
     
-	// $scope.updateProfile = ProfileFactory.updateProfile;
-
     AuthService.getLoggedInUser()
     .then(function (user) {
         $scope.user = user; 
-        return user
+        return user;
     })
     .then(function (theUser) {
         ProfileFactory.getOrderHistory(theUser.id)
         .then(function (orders) {
-            $scope.orders = orders
-        })
-    })
+            $scope.orders = orders;
+        });
+    });
 
     $scope.updateProfile = ProfileFactory.updateProfile;
-  
+
 });
-
-
-app.factory('ProfileFactory', function ($http, $log) {
-	var obj = {};
-	// var cachedUser = [];
-
-	obj.updateProfile = function(profile, profileId) {
-		return $http.put('/api/users/' + profileId, profile)
-		.then(function (updated) {
-			console.log(updated.data);
-			// angular.copy(cachedUser, updated.data);
-			return updated.data;
-		})
-		.catch($log.error)
-	};
-
-    obj.getOrderHistory = function (userId) {
-        return $http.get('/api/orders/history' + userId)
-        .then(function (updated) {
-
-            console.log('order history', updated.data);
-            return updated.data;
-
-        })
-        .catch($log.error);
-    };
-
-	return obj;
-});
-
