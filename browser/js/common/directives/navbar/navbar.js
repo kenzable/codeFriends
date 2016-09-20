9b91f2562f66e22433b6a4dd7a7f208e8228f5ad
Angela Choi <angela.minchoi@gmail.com>
2016-09-20 09:20:08 -0400

    Orderhistory (#41)
    * profile history working
    
    * deleted bower_components from github
    
    * deleted members only page and made user profile user only page
    
    *organized the navbar

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -13,9 +13,7 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state, 
             scope.items = [
                 { label: 'Home', state: 'home' },
                 { label: 'About', state: 'about' },
-                { label: 'Checkout', state: 'cart' },
-                { label: 'Members Only', state: 'membersOnly', auth: true },
-                { label: 'User Only', state: 'profile({id: user.id})'}
+                { label: 'Checkout', state: 'cart' }
             ];
 
             scope.user = null;
